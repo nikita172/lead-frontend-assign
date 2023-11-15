712a00b88c0ba716b31fc673298a6cef386067e1
nikita-rawat <nikitarwt789@gmail.com>
2023-11-15 17:22:12 +0530

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lead-frontend-assign/",
   plugins: [react()],
-  base: "/lead-frontend-assign/"
 })
